370e24722d0ad8704a2fdfe800ed84638878706f
mohanbaabhu001 <mohanbaabhusmourish@gmail.com>
2025-05-09 17:19:41 +0530

    Delete scripts directory

--- scripts/set_permissions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Setting file permissions..."
-
-# Use www-data instead of nginx on Ubuntu
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
-
-echo "Permission setting completed."
