4c8a3e745858cca5126a58b1e8b51cc243465704
mohanbaabhu001 <mohanbaabhusmourish@gmail.com>
2025-05-09 15:59:45 +0530

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -2,8 +2,8 @@
 echo "Installing NGINX..."
 
 # Run as root (CodeDeploy runs as root per your appspec)
-apt-get update -y
+sudo apt-get update -y
 
-apt-get install -y nginx
+sudo apt-get install -y nginx
 
 echo "NGINX installation completed."
